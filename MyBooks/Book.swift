37c36b7285f7ba97a2519b5297e9b8410193e08c
Ahmed Ben Henda <ahmed.bnhenda@gmail.com>
2024-05-10 10:03:41 +0100

    Tuto 1
    https://youtu.be/CAr_1kcf2_c?si=q7MefoEIlkcMkmVx

---
+++ MyBooks/Book.swift
@@ -0,0 +1,70 @@
+//
+//  Book.swift
+//  MyBooks
+//
+//  Created by Anypli M1 Air on 9/5/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@Model
+class Book {
+    init(
+        title: String,
+        author: String,
+        dateAdded: Date = Date.now,
+        dateStarted: Date = Date.distantPast,
+        dateCompleted: Date = Date.distantPast,
+        summary: String = "",
+        rating: Int? = nil,
+        status: Status = .onShelf
+    ) {
+        self.title = title
+        self.author = author
+        self.dateAdded = dateAdded
+        self.dateStarted = dateStarted
+        self.dateCompleted = dateCompleted
+        self.summary = summary
+        self.rating = rating
+        self.status = status
+    }
+    
+    var title: String
+    var author: String
+    var dateAdded: Date
+    var dateStarted: Date
+    var dateCompleted: Date
+    var summary: String
+    var rating: Int?
+    var status: Status
+    
+    var icon: Image {
+        switch status {
+        case .onShelf:
+            Image(systemName: "checkmark.diamond.fill")
+        case .inProgress:
+            Image(systemName: "book.fill")
+        case .completed:
+            Image(systemName: "books.vertical.fill")
+        }
+    }
+}
+
+enum Status: Int, Codable, Identifiable, CaseIterable {
+    case onShelf, inProgress, completed
+    var id: Self {
+        self
+    }
+    
+    var descr: String {
+        switch self {
+        case .onShelf:
+            "On Shelf"
+        case .inProgress:
+            "In Progress"
+        case .completed:
+            "Completed"
+        }
+    }
+}
---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,23 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Anypli M1 Air on 9/5/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BookListView()
+        }
+        .modelContainer(for: Book.self)
+    }
+    
+    init() {
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
+    }
+}
